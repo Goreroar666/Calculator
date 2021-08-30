e38137b79d6b33ca1b14084261456fb00cf06167
Kasper Mennink <kasper.mennink@hotmail.com>
2021-08-30 21:57:54 +0200

    Added ids to buttons and declared globals.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,25 @@
+const zero = document.getElementById("0");
+const one = document.getElementById("1");
+const two = document.getElementById("2");
+const three = document.getElementById("3");
+const four = document.getElementById("4");
+const five = document.getElementById("5");
+const six = document.getElementById("6");
+const seven = document.getElementById("7");
+const eight = document.getElementById("8");
+const nine = document.getElementById("9");
+const AC = document.getElementById("AC");
+const minus = document.getElementById("+/-");
+const perc = document.getElementById("%");
+const slash = document.getElementById("/");
+const mult = document.getElementById("*");
+const plus = document.getElementById("+");
+const dot = document.getElementById(".");
+const operateBtn = document.getElementById("=");
+const subtrBtn = document.getElementById("-");
+
+let screenArray = [];
+
 function add(x, y) {
 	return x + y;
 };
@@ -38,3 +60,5 @@ return add(x, y);
   return divide(x, y);
 }
 };
+
+one.addEventListener('click', )
